55a1de2b5d48fb8c2852fdba0f0ac5ba93fb542f
adrian <a_lost_shadow@imap.cc>
2016-02-22 20:54:55 -0800

    finished renaming myproject to shadowcon

--- setup.py
+++ setup.py
@@ -3,19 +3,12 @@
 from setuptools import setup
 
 setup(
-    # GETTING-STARTED: set your app name:
     name='ShadowConWebsite',
-    # GETTING-STARTED: set your app version:
     version='1.0',
-    # GETTING-STARTED: set your app description:
     description='ShadowConWebsite',
-    # GETTING-STARTED: set author name (your name):
     author='Adrian Barnes',
-    # GETTING-STARTED: set author email (your email):
     author_email='a_lost_shadow@imap.cc',
-    # GETTING-STARTED: set author url (your url):
     url='http://www.python.org/sigs/distutils-sig/',
-    # GETTING-STARTED: define required django version:
     install_requires=[
         'Django==1.9.2'
     ],
