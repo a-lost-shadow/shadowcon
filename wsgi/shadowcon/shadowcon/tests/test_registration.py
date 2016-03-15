df658cee0276adce55cbb062ea88fbfe35c7f9e3
adrian <a_lost_shadow@imap.cc>
2016-03-14 23:18:26 -0700

    Password Reset/Change Template Tests

--- wsgi/shadowcon/shadowcon/tests/test_registration.py
+++ wsgi/shadowcon/shadowcon/tests/test_registration.py
@@ -94,3 +94,69 @@ class NewUserRegistrationTest(SectionCheckMixIn, TestCase):
         with self.assertRaises(TemplateDoesNotExist) as e:
             self.client.get(link)
         self.assertEquals(e.exception.message, "registration/activate.html")
+
+
+# Mainly just want to test that our templates are being loaded
+class PasswordResetChangeTest(SectionCheckMixIn, TestCase):
+    fixtures = ['auth', 'initial']
+
+    def setUp(self):
+        self.client = Client()
+
+    def run_test(self, key):
+        response = self.client.get(reverse(key))
+        self.assertSectionContains(response, "ShadowCon 2016", "title")
+        return response
+
+    def test_login(self):
+        response = self.run_test('login')
+        self.assertSectionContains(response, '<p><a href="%s">%s</a></p>' % (reverse('password_reset'),
+                                                                             'Lost password\\?'),
+                                   'section id="main" role="main"', '/section')
+        self.assertSectionContains(response, '<p><a href="%s">%s</a></p>' % (reverse('con:new_user'),
+                                                                             'Create New Account'),
+                                   'section id="main" role="main"', '/section')
+
+    def test_logout(self):
+        response = self.run_test('logout')
+        self.assertSectionContains(response, 'You have successfully logged out',
+                                   'section id="main" role="main"', '/section')
+
+    def test_password_change(self):
+        self.client.login(username="user", password="123")
+        response = self.run_test('password_change')
+        self.assertSectionContains(response, 'Enter new password', 'section id="main" role="main"', '/section')
+
+    def test_password_change_done(self):
+        self.client.login(username="user", password="123")
+        response = self.run_test('password_change_done')
+        self.assertSectionContains(response, '<h2>Password Changed</h2>', 'section id="main" role="main"', '/section')
+
+    def test_password_reset(self):
+        response = self.run_test('password_reset')
+        self.assertSectionContains(response, 'Forgotten your password\\? Enter your',
+                                   'section id="main" role="main"', '/section')
+
+    def test_password_reset_done(self):
+        response = self.run_test('password_reset_done')
+        self.assertSectionContains(response, 'Password reset sent', 'section id="main" role="main"', '/section')
+
+    def test_password_reset_confirm_invalid_link(self):
+        response = self.client.get(reverse('password_reset_confirm', args=["invalid", "4a8-cd1bf13135ee4cae7176"]))
+        self.assertSectionContains(response, "ShadowCon 2016", "title")
+        self.assertSectionContains(response, 'The password reset link was invalid',
+                                   'section id="main" role="main"', '/section')
+
+    def test_password_reset_confirm_valid_link(self):
+        self.client.post(reverse('password_reset'), {"email": "user@na.com"})
+
+        self.assertEquals(len(mail.outbox), 1)
+        email = mail.outbox[0]
+        start = str(email.body).index('/reset')
+        stop = str(email.body).index('Your username, in case you\'ve forgotten: user')
+        link = str(email.body)[start:stop].strip()
+
+        response = self.client.get(link)
+        self.assertSectionContains(response, "ShadowCon 2016", "title")
+        self.assertSectionContains(response, 'Please enter your new password twice',
+                                   'section id="main" role="main"', '/section')
